78344aad53b8bcaba2680fee071958ac55ebf4e1
Clint Edwards <cedwards1080@gmail.com>
2021-08-31 15:29:06 -0600

    adding the backend to add a student

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,7 +1,22 @@
+const students = []
 
 module.exports = {
-    // filePath: (req, res) => {
-    //     res.sendFile(path.join(__dirname, "../public/index.html"));
-    //     rollbar.info("html file served successfully");
-    // }
+        addStudent: (req, res) => {
+    let { name } = req.body;
+    name = name.trim();
+
+    const index = students.findIndex((studentName) => studentName === name);
+
+    if (index === -1 && name !== "") {
+        students.push(name);
+        rollbar.log('Student added succussfully', {author: 'Clint'})
+        res.status(200).send(students);
+    } else if (name === "") {
+        rollbar.error('No name given')
+        res.status(400).send("must provide a name.");
+    } else {
+        rollbar.error('Student already exists')
+        res.status(400).send("that student already exists");
+    }
+    }
 }
