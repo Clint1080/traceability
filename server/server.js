c6530418ee4589e9eecabd35e8b62437e6ae1e8d
Clint Edwards <cedwards1080@gmail.com>
2021-08-31 15:59:23 -0600

    trying to get back to working

--- server/server.js
+++ server/server.js
@@ -14,36 +14,36 @@ const rollbar = new Rollbar({
   captureUnhandledRejections: true,
 });
 
-// const ctrl = require('./controllers/controller')
-const students = []
+const ctrl = require('./controllers/controller')
+// const students = []
 
-// app.get('/', ctrl.filePath)
+app.get('/', ctrl.filePath)
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
   rollbar.info("html file served successfully");
 });
 
-// app.post('/api/student', ctrl.addStudent)
+app.post('/api/student', ctrl.addStudent)
 
-app.post('/api/student', (req, res) => {
-    let { name } = req.body;
-    name = name.trim();
+// app.post('/api/student', (req, res) => {
+//     let { name } = req.body;
+//     name = name.trim();
 
-    const index = students.findIndex((studentName) => studentName === name);
+//     const index = students.findIndex((studentName) => studentName === name);
 
-    if (index === -1 && name !== "") {
-        students.push(name);
-        rollbar.log("Student added succussfully", { author: "Clint" });
-        res.status(200).send(students);
-    } else if (name === "") {
-        rollbar.error("No name given");
-        res.status(400).send("must provide a name.");
-    } else {
-        rollbar.error("Student already exists");
-        res.status(400).send("that student already exists");
-    }
-})
+//     if (index === -1 && name !== "") {
+//         students.push(name);
+//         rollbar.log("Student added succussfully", { author: "Clint" });
+//         res.status(200).send(students);
+//     } else if (name === "") {
+//         rollbar.error("No name given");
+//         res.status(400).send("must provide a name.");
+//     } else {
+//         rollbar.error("Student already exists");
+//         res.status(400).send("that student already exists");
+//     }
+// })
 
 
 
